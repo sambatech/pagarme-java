760cf36d6ff9962483b2a51908305d7f8e2a83ae
Daniel Ferreira <daniel.calibeta@gmail.com>
2016-09-23 17:53:22 -0300

    Fix deployment script. Bump version.

--- deploy-to-maven.sh
+++ deploy-to-maven.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+mvn clean
+
 mvn package
 
 mvn source:jar
 mvn javadoc:jar
 
-mvn gpg:sign-and-deploy-file -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2 -DrepositoryId=ossrh -DpomFile=pom.xml -Dfile=target/pagarme-java-1.1.jar 
-mvn gpg:sign-and-deploy-file -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2 -DrepositoryId=ossrh -DpomFile=pom.xml -Dfile=target/pagarme-java-1.1-sources.jar -Dclassifier=sources
-mvn gpg:sign-and-deploy-file -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2 -DrepositoryId=ossrh -DpomFile=pom.xml -Dfile=target/pagarme-java-1.1-javadoc.jar -Dclassifier=javadoc
+echo "Please confirm the deployment version."
+read DEPVERSION
+
+mvn gpg:sign-and-deploy-file -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2 -DrepositoryId=ossrh -DpomFile=pom.xml -Dfile=target/pagarme-java-${DEPVERSION}.jar 
+mvn gpg:sign-and-deploy-file -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2 -DrepositoryId=ossrh -DpomFile=pom.xml -Dfile=target/pagarme-java-${DEPVERSION}-sources.jar -Dclassifier=sources
+mvn gpg:sign-and-deploy-file -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2 -DrepositoryId=ossrh -DpomFile=pom.xml -Dfile=target/pagarme-java-${DEPVERSION}-javadoc.jar -Dclassifier=javadoc
