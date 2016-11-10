62d9abe3de8a77e60399a586e05e68c26c2104ee
henriquekano <henriquekano@hotmail.com>
2016-11-10 19:02:46 -0200

    Indentation from tab to 4 spaces

--- src/test/java/me/pagarme/factory/BankAccountFactory.java
+++ src/test/java/me/pagarme/factory/BankAccountFactory.java
@@ -4,17 +4,17 @@ import me.pagar.model.BankAccount;
 
 public class BankAccountFactory {
 
-	public BankAccount create(){
-		BankAccount bankAccount = new BankAccount();
-		bankAccount.setAgencia("0192");
-		bankAccount.setAgenciaDv("0");
-		bankAccount.setBankCode("341");
-		bankAccount.setConta("03245");
-		bankAccount.setContaDv("0");
-		bankAccount.setAgenciaDv("0");
-		bankAccount.setLegalName("Conta teste");
-		bankAccount.setDocumentNumber("18344334799");
-		
-		return bankAccount;
-	}
+    public BankAccount create(){
+        BankAccount bankAccount = new BankAccount();
+        bankAccount.setAgencia("0192");
+        bankAccount.setAgenciaDv("0");
+        bankAccount.setBankCode("341");
+        bankAccount.setConta("03245");
+        bankAccount.setContaDv("0");
+        bankAccount.setAgenciaDv("0");
+        bankAccount.setLegalName("Conta teste");
+        bankAccount.setDocumentNumber("18344334799");
+        
+        return bankAccount;
+    }
 }
