de7b1804e0cbba6d1ee58ba6ac8ee27e1577d487
henriquekano <henrique.kano@pagar.me>
2016-11-10 14:32:44 -0200

    Added Split rules to POST /transactions endpoint. Added tests factories.

---
+++ src/test/java/me/pagarme/factory/RecipientFactory.java
@@ -0,0 +1,20 @@
+package me.pagarme.factory;
+
+import me.pagar.model.BankAccount;
+import me.pagar.model.Recipient;
+import me.pagar.model.Recipient.TransferInterval;
+
+public class RecipientFactory {
+
+	private BankAccountFactory bankAccountFactory = new BankAccountFactory();
+	
+	public Recipient create(){
+		BankAccount bankAccount = bankAccountFactory.create();
+		Recipient recipient = new Recipient();
+		recipient.setBankAccount(bankAccount);
+		recipient.setTransferDay(1);
+		recipient.setTransferEnabled(true);
+		recipient.setTransferInterval(TransferInterval.WEEKLY);
+		return recipient;
+	}
+}
