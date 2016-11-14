1f5ada8404ab4082c3d82c9ccfb626eadf170273
henriquekano <henrique.kano@pagar.me>
2016-11-14 09:10:26 -0200

    Added recipient tests

--- src/test/java/me/pagarme/BankAccountTest.java
+++ src/test/java/me/pagarme/BankAccountTest.java
@@ -10,18 +10,18 @@ import me.pagarme.factory.BankAccountFactory;
 
 public class BankAccountTest extends BaseTest{
 
-	public BankAccountFactory bankAccountFactory = new BankAccountFactory();
-	
-	public BankAccountTest() {
-		super.setUp();
-	}
-	
+    public BankAccountFactory bankAccountFactory = new BankAccountFactory();
+    
+    public BankAccountTest() {
+        super.setUp();
+    }
+    
     @Test
     public void testCreateBackAccount() {
 
         try {
-        	
-        	BankAccount bankAccount = bankAccountFactory.create();
+            
+            BankAccount bankAccount = bankAccountFactory.create();
             BankAccount savedBankAccount = bankAccount.save();
             
             Assert.assertNotNull(savedBankAccount.getId());
@@ -42,13 +42,13 @@ public class BankAccountTest extends BaseTest{
 
         try {
 
-        	BankAccount bankAccount = bankAccountFactory.create();
-        	BankAccount savedBankAccount = bankAccount.save();
-        	BankAccount foundBanckAccount = new BankAccount().find(bankAccount.getId());
-        	
-        	Assert.assertNotNull(foundBanckAccount.getId());
-        	Assert.assertEquals(savedBankAccount.getId(), foundBanckAccount.getId());
-        	Assert.assertEquals(savedBankAccount.getAgencia(), BankAccountFactory.DEFAULT_AGENCIA);
+            BankAccount bankAccount = bankAccountFactory.create();
+            BankAccount savedBankAccount = bankAccount.save();
+            BankAccount foundBanckAccount = new BankAccount().find(bankAccount.getId());
+            
+            Assert.assertNotNull(foundBanckAccount.getId());
+            Assert.assertEquals(savedBankAccount.getId(), foundBanckAccount.getId());
+            Assert.assertEquals(savedBankAccount.getAgencia(), BankAccountFactory.DEFAULT_AGENCIA);
             Assert.assertEquals(savedBankAccount.getAgenciaDv(), BankAccountFactory.DEFAULT_AGENCIA_DV);
             Assert.assertEquals(savedBankAccount.getConta(), BankAccountFactory.DEFAULT_CONTA);
             Assert.assertEquals(savedBankAccount.getContaDv(), BankAccountFactory.DEFAULT_CONTA_DV);
@@ -66,18 +66,18 @@ public class BankAccountTest extends BaseTest{
 
         try {
 
-        	BankAccount bankAccount = bankAccountFactory.create();
-        	bankAccount.save();
-        	BankAccount otherBankAccount = bankAccountFactory.create();
-        	otherBankAccount.save();
-        	
+            BankAccount bankAccount = bankAccountFactory.create();
+            bankAccount.save();
+            BankAccount otherBankAccount = bankAccountFactory.create();
+            otherBankAccount.save();
+            
             Collection<BankAccount> currentBanckAccount = bankAccount.findCollection(2,0);
 
             Assert.assertEquals(currentBanckAccount.size(), 2);
 
             for (BankAccount bankAccountVar : currentBanckAccount) {
-            	Assert.assertNotNull(bankAccountVar.getId());
-            	Assert.assertEquals(bankAccountVar.getAgencia(), BankAccountFactory.DEFAULT_AGENCIA);
+                Assert.assertNotNull(bankAccountVar.getId());
+                Assert.assertEquals(bankAccountVar.getAgencia(), BankAccountFactory.DEFAULT_AGENCIA);
                 Assert.assertEquals(bankAccountVar.getAgenciaDv(), BankAccountFactory.DEFAULT_AGENCIA_DV);
                 Assert.assertEquals(bankAccountVar.getConta(), BankAccountFactory.DEFAULT_CONTA);
                 Assert.assertEquals(bankAccountVar.getContaDv(), BankAccountFactory.DEFAULT_CONTA_DV);
@@ -85,7 +85,6 @@ public class BankAccountTest extends BaseTest{
                 Assert.assertEquals(bankAccountVar.getDocumentNumber(), BankAccountFactory.DEFAULT_DOCUMENT_NUMBER);
                 Assert.assertEquals(bankAccountVar.getLegalName(), BankAccountFactory.DEFAULT_LEGAL_NAME);
             }
-            
         } catch (Exception exception) {
             throw new UnsupportedOperationException(exception);
         }
