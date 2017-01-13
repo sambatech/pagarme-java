41f31c105ce37e170603244a7074130387c08075
Henrique Kano <henriquekano@hotmail.com>
2017-01-13 14:05:38 -0200

    Update deploy-to-maven script

--- deploy-to-maven.sh
+++ deploy-to-maven.sh
@@ -7,7 +7,7 @@ mvn package
 mvn source:jar
 mvn javadoc:jar
 
-DEPVERSION=$(git describe --tags --always HEAD)
+DEPVERSION=$(git describe --tags --abbrev=0 --always HEAD)
 
 mvn gpg:sign-and-deploy-file -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2 -DrepositoryId=ossrh -DpomFile=pom.xml -Dfile=target/pagarme-java-${DEPVERSION}.jar 
 mvn gpg:sign-and-deploy-file -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2 -DrepositoryId=ossrh -DpomFile=pom.xml -Dfile=target/pagarme-java-${DEPVERSION}-sources.jar -Dclassifier=sources
