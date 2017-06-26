6682bf3d4cced9caaa05db5f325921f0603dcd6a
victor messina <victormessina95@hotmail.com>
2017-06-26 14:44:27 -0300

    create setPlanId in subscription class

--- src/main/java/me/pagar/model/Subscription.java
+++ src/main/java/me/pagar/model/Subscription.java
@@ -161,6 +161,10 @@ public class Subscription extends PagarMeModel<String> {
         this.cardHash = cardHash;
     }
 
+    public void setPlanId(String planId){
+        this.planId = planId;
+    }
+
     public String getCardHash() {
         return cardHash;
     }
