16d0fb7b1c753661f47d3d22fccfdde8ab3a1270
victor messina <victormessina95@hotmail.com>
2017-06-30 15:37:39 -0300

    Create splitTest in SubscriptionTest

--- src/test/java/me/pagarme/SubscriptionTest.java
+++ src/test/java/me/pagarme/SubscriptionTest.java
@@ -1,5 +1,7 @@
 package me.pagarme;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Iterator;
 
 import org.junit.Assert;
@@ -11,12 +13,15 @@ import me.pagar.model.Card;
 import me.pagar.model.Customer;
 import me.pagar.model.PagarMeException;
 import me.pagar.model.Plan;
+import me.pagar.model.Recipient;
+import me.pagar.model.SplitRule;
 import me.pagar.model.Subscription;
 import me.pagar.model.Transaction;
 import me.pagar.model.Transaction.PaymentMethod;
 import me.pagarme.factory.CardFactory;
 import me.pagarme.factory.CustomerFactory;
 import me.pagarme.factory.PlanFactory;
+import me.pagarme.factory.RecipientFactory;
 import me.pagarme.factory.SubscriptionFactory;
 import static org.hamcrest.CoreMatchers.instanceOf;
 
@@ -26,8 +31,10 @@ public class SubscriptionTest extends BaseTest {
     private SubscriptionFactory subscriptionFactory = new SubscriptionFactory();
     private CustomerFactory customerFactory = new CustomerFactory();
     private CardFactory cardFactory = new CardFactory();
+    private RecipientFactory recipientFactory = new RecipientFactory();
 
     private Plan defaultPlan;
+    private Plan defaultPlan2;
     private Customer defaultCustomer;
     private Card defaultCard;
 
@@ -39,6 +46,9 @@ public class SubscriptionTest extends BaseTest {
 
         defaultPlan = planFactory.create();
         defaultPlan.save();
+
+        defaultPlan2 = planFactory.createPlanWithoutTrialDays();
+        defaultPlan2.save();
 
         defaultCard = cardFactory.create();
         defaultCard.save();
@@ -137,5 +147,41 @@ public class SubscriptionTest extends BaseTest {
         subscription.save();
 
         Assert.assertNotNull(subscription.getPostbackUrl());
+    }
+
+    @Test
+    public void testSplitSubscription() throws Throwable {
+
+        Subscription subscription = subscriptionFactory.createCreditCardSubscription(defaultPlan2.getId(),defaultCard.getId(), defaultCustomer);
+        Collection<SplitRule> splitRules = new ArrayList<SplitRule>();
+
+        Recipient recipient1 = recipientFactory.create();
+        recipient1.save();
+
+        SplitRule splitRule = new SplitRule();
+        splitRule.setRecipientId(recipient1.getId());
+        splitRule.setPercentage(50);
+        splitRule.setLiable(true);
+        splitRule.setChargeProcessingFee(true);
+        splitRules.add(splitRule);
+
+        Recipient recipient2  = recipientFactory.create();
+        recipient2.save();
+
+        SplitRule splitRule2 = new SplitRule();
+        recipient2.save();
+        splitRule2.setRecipientId(recipient2.getId());
+        splitRule2.setPercentage(50);
+        splitRule2.setLiable(true);
+        splitRule2.setChargeProcessingFee(true);
+        splitRules.add(splitRule2);
+
+        subscription.setSplitRules(splitRules);
+        subscription.save();
+
+        Transaction foundTransaction = new Transaction().find(subscription.getCurrentTransaction().getId());
+
+        Collection<SplitRule> foundSplitRules = foundTransaction.getSplitRules();
+        Assert.assertEquals(splitRules.size(), foundSplitRules.size());
     }
 }
